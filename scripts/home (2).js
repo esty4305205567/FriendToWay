02081eecadf1a16899abda481e77046ba8033127
esty4305205567 <esty43052@gmail.com>
2026-07-16 19:41:27 +0300

    Update popup redirection from home to index

--- scripts/home (2).js
+++ scripts/home (2).js
@@ -2,7 +2,7 @@ let form = document.querySelector("form");
 const main1 = document.querySelector('main')
 form.onsubmit = (e) => {
     e.preventDefault();
-    pop_up(main1, "תודה!", "fa-star", "לחזרה", "נציגנו יחזרו אליכם", false, "", "./home.html")
+    pop_up(main1, "תודה!", "fa-star", "לחזרה", "נציגנו יחזרו אליכם", false, "", "./index.html")
     form.reset()
 }
 let logA = document.querySelector("#logA")
